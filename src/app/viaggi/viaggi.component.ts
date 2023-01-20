ccabe6c122759567f0c1dc1dbc51ffbffbc20411
nicolasventurini <117444138+nicolasventurini@users.noreply.github.com>
2023-01-20 10:49:19 +0100

    2 files modified

--- src/app/viaggi/viaggi.component.ts
+++ src/app/viaggi/viaggi.component.ts
@@ -8,7 +8,7 @@ import { ILocalita } from './localita';
 })
 export class LocalitaComponent {
   paese: string;
-  stato: string;
+  nazione: string;
   immagine: string;
   mostraImmagine: boolean = false;
   listaFilter:string
@@ -17,19 +17,19 @@ export class LocalitaComponent {
 localita: ILocalita[] = [
   {
     paese: 'Riva del Garda',
-    stato: 'Italia',
+    nazione: 'Italia',
     immagine:
       'https://www.gardatrentino.it/website_images/Guida/MICE/image-thumb__5065__navigationBackground/Fiere_Congressi_RFC_Mice_Aperitivo_Riva_del_Garda.jpg',
   },
   {
     paese: 'Halifax',
-    stato: 'Canada',
+    nazione: 'Canada',
     immagine:
       'https://upload.travelawaits.com/ta/uploads/2021/04/2b2cac9334d02796fc53224800ba02b2cac.jpg',
   },
   {
     paese: 'Berlino',
-    stato: 'Germania',
+    nazione: 'Germania',
     immagine:
       'https://i0.wp.com/hierdadort.de/wp-content/uploads/2020/03/mural-oranienstra%C3%9Fe-berlin.jpg?resize=1024%2C851&ssl=1',
   },
